50dfafc175f7d2de2dcc23cb36d69ae81a4c8a15
PhucThanh <tranthanhphuc042@gmail.com>
2021-01-02 13:11:55 +0700

    delete, count cart

--- src/pages/Cart/PlusMinusButton.js
+++ src/pages/Cart/PlusMinusButton.js
@@ -1,39 +1,38 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import "./PlusMinusButton.css";
 class PlusMinusButton extends Component{
     constructor(props) {
         super();};
 
         render(){
-            return(<div class="input-group">
+            return(<div class="input-group" style={{maxWidth:'10em'}}>
             <span class="input-group-btn">
               <button
                 type="button"
                 class="btn btn-default btn-number"
-                disabled="disabled"
                 data-type="minus"
-                data-field="quant[1]"
+                onClick={this.props.onCountChange(this.props.item.product_id,this.props.item.counting-1)}
+                disabled={this.props.item.counting==0?true:false}
               >
-                <FontAwesomeIcon icon={faMinus} />
+                <FontAwesomeIcon icon={faMinus} size="xs"/>
               </button>
             </span>
             <input
-              type="text"
-              name="quant[1]"
+              type="number"
               class="form-control input-number"
-              value={this.props.count}
-              min="1"
-              max="10"
+              value={this.props.item.counting}
+              onChange={this.props.onCountChange(this.props.item.product_id)}
             />
             <span class="input-group-btn">
               <button
                 type="button"
                 class="btn btn-default btn-number"
                 data-type="plus"
-                data-field="quant[1]"
+                onClick={this.props.onCountChange(this.props.item.product_id,this.props.item.counting+1)}
               >
-                <FontAwesomeIcon icon={faPlus} />
+                <FontAwesomeIcon icon={faPlus} size="xs"/>
               </button>
             </span>
           </div>
