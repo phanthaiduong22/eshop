1d37338566288476e3bdac068d819d6432cf2d35
PhucThanh <tranthanhphuc042@gmail.com>
2021-01-06 20:26:28 +0700

    List Order

---
+++ src/pages/SellerOrders/OrdersList.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import Select from "react-select";
+
+class OrderList extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      selectedOption: null,
+    };
+  }
+  handleChange = id => (selectedOption) => {
+      console.log("id:",id);
+    console.log(`Option selected:`, selectedOption.target.value);
+    this.props.pushOrder({id:id, status:selectedOption.target.value});
+  };
+  render() {
+    console.log(this.props.orders);
+    return Object.keys(this.props.orders).map((key, i) => {
+      if (this.props.orders[key].detail.status != this.props.type) {
+        return null;
+      }
+      //return null
+      return (
+        <div class="card" key={i}>
+          <div class="card-body">
+            <h6 class="card-title" style={{ float: "left" }}>
+              Đơn hàng #{key}
+            </h6>
+
+            <div class="form-group" style={{ float: "right", minWidth: "8em" }}>
+              <label for=""></label>
+              <select
+              class="form-control"
+                value={this.props.orders[key].detail.status}
+                onChange={this.handleChange(this.props.orders[key].detail.id)}
+              >
+                <option value={0}>Chưa xác nhận</option>
+                <option value={1}>Đã xác nhận</option>
+                <option value={2}>Đang giao</option>
+                <option value={3}>Đã giao</option>
+              </select>
+            </div>
+
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Sản phẩm</th>
+                  <th scope="col">Giá tiền</th>
+                  <th scope="col">Số lượng</th>
+                  <th scope="col">Tổng tiền</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.props.orders[key].products.map((order, index) => {
+                  return (
+                    <tr>
+                      <th scope="row">
+                        {order.image_url ? (
+                          <img
+                            src={order.image_url}
+                            alt="pic"
+                            className="img-responsive full-width"
+                            style={{ maxHeight: "5em", maxWidth: "5em" }}
+                          />
+                        ) : (
+                          <img
+                            src="https://cdn0.iconfinder.com/data/icons/picture-sharing-sites-solid/32/No_Image-128.png"
+                            alt="pic"
+                            className="img-responsive full-width"
+                            style={{ maxHeight: "5em" }}
+                          />
+                        )}
+                        <span style={{ marginLeft: "1em" }}>
+                          {" "}
+                          {order.product_name}
+                        </span>
+                      </th>
+                      <td>{order.price}</td>
+                      <td>{order.counting}</td>
+                      <td>{order.price * order.counting}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      );
+    });
+  }
+}
+
+export default OrderList;
--- src/pages/SellerOrders/SellerOrders.js
+++ src/pages/SellerOrders/SellerOrders.js
@@ -1,41 +1,167 @@
 import React, { Component } from "react";
+import callAPI from "../../utils/apiCaller";
 import SellerSideBar from "../../components/SellerSideBar/SellerSideBar";
+import "./SellerOrders.css";
+import OrderList from "./OrdersList"
 class SellerOrders extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      orders: [],
+      token: "",
+    };
+  }
+  getOrder = () => {
+    if (this.state.token) {
+      callAPI("/storegetorder", "GET", null, this.state.token)
+        .then((res) => {
+          console.log(res.data);
+          this.setState({ orders: res.data });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  pushOrder=(order)=>{
+    if (this.state.token) {
+      callAPI("/pushorder", "POST", order, this.state.token)
+        .then((res) => {
+          if (res.data == "updated OK") {
+            this.getOrder();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
+  componentDidMount = () => {
+    let token_ = localStorage.getItem("token");
+    this.setState({ token: token_ }, function () {
+      this.getOrder();
+    });
+  };
+ 
+
   render() {
+    let orders = {
+
+    };
+
+        this.state.orders.map((order, index) => {
+          if (orders[order["order_id"]] == null) {
+            orders[order["order_id"]]={};
+            orders[order["order_id"]].products = [];
+            orders[order["order_id"]].detail={status:order["status"],id:order["order_id"]};
+          }
+          orders[order["order_id"]].products.push(order);
+        });
+    
+        console.log(orders);
+
     return (
       <div className="container-fluid ">
         <div className="row m-5 ">
           <SellerSideBar />
-          <div className="col">
-            <div class="card text-black bg-secondary">
+          <div className="col-md">
+            <div class="card">
               <div class="card-body">
-                <div class="card text-black bg-white">
-                  <div class="card-body">
-                    <h4 class="card-title">Đơn hàng</h4>
-                    <table class="table">
-                      <thead>
-                        <tr>
-                          <th>Tên sản phẩm</th>
-                          <th>Số lượng</th>
-                          <th>Giá</th>
-                          <th>Tổng</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td scope="row">Dầu gọi đầu</td>
-                          <td>5</td>
-                          <td>10.000</td>
-                          <td>50.000</td>
-                        </tr>
-                        <tr>
-                          <td scope="row">Dầu gọi đầu</td>
-                          <td>5</td>
-                          <td>10.000</td>
-                          <td>50.000</td>
-                        </tr>
-                      </tbody>
-                    </table>
+                <h4 class="card-title">Danh sách đơn hàng</h4>
+
+                <ul
+                  class="nav nav-pills"
+                  id="myTab"
+                  role="tablist"
+                  style={{ marginBottom: "2em" }}
+                >
+                  <li class="nav-item">
+                    <a
+                      class="nav-link active"
+                      id="home-tab"
+                      data-toggle="tab"
+                      href="#home"
+                      role="tab"
+                      aria-controls="home"
+                      aria-selected="true"
+                    >
+                      Chưa xác nhận
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      id="dxn-tab"
+                      data-toggle="tab"
+                      href="#dxn"
+                      role="tab"
+                      aria-controls="dxn"
+                      aria-selected="false"
+                    >
+                      Đã xác nhận
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      id="profile-tab"
+                      data-toggle="tab"
+                      href="#profile"
+                      role="tab"
+                      aria-controls="profile"
+                      aria-selected="false"
+                    >
+                      Đang giao
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      id="contact-tab"
+                      data-toggle="tab"
+                      href="#contact"
+                      role="tab"
+                      aria-controls="contact"
+                      aria-selected="false"
+                    >
+                      Đã giao
+                    </a>
+                  </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                  <div
+                    class="tab-pane fade show active"
+                    id="home"
+                    role="tabpanel"
+                    aria-labelledby="home-tab"
+                  >
+                   <OrderList orders={orders} type={0} pushOrder={this.pushOrder}/>
+                  </div>
+                  <div
+                    class="tab-pane fade"
+                    id="dxn"
+                    role="tabpanel"
+                    aria-labelledby="dxn-tab"
+                  >
+                    <OrderList orders={orders} type={1} pushOrder={this.pushOrder}/>
+                  </div>
+                  <div
+                    class="tab-pane fade"
+                    id="profile"
+                    role="tabpanel"
+                    aria-labelledby="profile-tab"
+                  >
+                    <OrderList orders={orders} type={2} pushOrder={this.pushOrder}/>
+                  </div>
+                  <div
+                    class="tab-pane fade"
+                    id="contact"
+                    role="tabpanel"
+                    aria-labelledby="contact-tab"
+                  >
+                    <OrderList orders={orders} type={3} pushOrder={this.pushOrder}/>
                   </div>
                 </div>
               </div>
